429f76f712f589540b17c49319afa7325c76e475
Alexander Standke <xanderstrike@gmail.com>
2015-06-16 09:18:00 -0700

    as - remove unnecessary health check method

--- test/rentlinx/test_client.rb
+++ test/rentlinx/test_client.rb
@@ -5,10 +5,4 @@ require_relative '../helper'
 # Test the Rentlinx Client
 class TestRentlinxClient < MiniTest::Test
   include SetupMethods
-
-  def test_health_check
-    VCR.use_cassette('test_client') do
-      Rentlinx::Client.health_check(@username, @password, @site_url)
-    end
-  end
 end
