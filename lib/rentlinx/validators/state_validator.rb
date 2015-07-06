05832966a1426390357290beb2d4b2fcec40ae1d
Alexander Standke <xanderstrike@gmail.com>
2015-07-06 16:35:51 -0700

    as - make validate methods private

--- lib/rentlinx/validators/state_validator.rb
+++ lib/rentlinx/validators/state_validator.rb
@@ -1,5 +1,7 @@
 module Rentlinx
   class StateValidator < BaseValidator
+    private
+
     def validate
       return if @value.nil? || @value == '' || @value.size == 2
       @error = "#{@value} is not a valid state, states must be two characters (CA)"
