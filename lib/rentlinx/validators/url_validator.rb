93290f6b54c4eb303bba657645da8dc8e9098c19
Nick Eidler <nickeidler@gmail.com>
2015-08-19 14:39:48 -0700

    ne - improve URL validations

--- lib/rentlinx/validators/url_validator.rb
+++ lib/rentlinx/validators/url_validator.rb
@@ -3,8 +3,15 @@ module Rentlinx
     private
 
     def validate
-      return if value_blank? || @value.match(%r{^https?:\/\/.*\.(com|net|org|us)\/.*$})
+      return if value_blank? || valid_url?(@value)
       @error = "#{@value} is not a valid URL"
+    end
+
+    def valid_url?(url)
+      uri = URI.parse(url)
+      uri.is_a?(URI::HTTP)
+    rescue URI::InvalidURIError
+      false
     end
   end
 end
--- test/validators/test_url_validator.rb
+++ test/validators/test_url_validator.rb
@@ -2,17 +2,17 @@ require_relative '../helper'
 
 class UrlValidatorTest < MiniTest::Test
   def test_validate
-    v = Rentlinx::UrlValidator.new('fakevhost.brokendomain')
+    v = Rentlinx::UrlValidator.new('wrong')
     refute v.valid?
-    assert_equal 'fakevhost.brokendomain is not a valid URL', v.error
+    assert_equal 'wrong is not a valid URL', v.error
 
-    v = Rentlinx::UrlValidator.new('duck')
+    v = Rentlinx::UrlValidator.new('http://$#*&^$@*&.com/')
     refute v.valid?
-    assert_equal 'duck is not a valid URL', v.error
+    assert_equal 'http://$#*&^$@*&.com/ is not a valid URL', v.error
 
-    v = Rentlinx::UrlValidator.new('development.localhost:3000/api/leads')
+    v = Rentlinx::UrlValidator.new('donaldgoose.4lyfe?')
     refute v.valid?
-    assert_equal 'development.localhost:3000/api/leads is not a valid URL', v.error
+    assert_equal 'donaldgoose.4lyfe? is not a valid URL', v.error
   end
 
   def test_processed_value
@@ -24,5 +24,8 @@ class UrlValidatorTest < MiniTest::Test
 
     v = Rentlinx::UrlValidator.new('mrgoose.appfolio.com/api/leads')
     assert_equal 'mrgoose.appfolio.com/api/leads', v.processed_value
+
+    v = Rentlinx::UrlValidator.new('https://www.randomcompany.co.jp/shiken_kanryou')
+    assert_equal 'https://www.randomcompany.co.jp/shiken_kanryou', v.processed_value
   end
 end
