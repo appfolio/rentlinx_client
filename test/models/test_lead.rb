74d771b3895451311e180792ed6b8204d21a9b41
Alexander Standke <xanderstrike@gmail.com>
2016-01-11 14:38:13 -0800

    as - add endpoint support for updating leads

---
+++ test/models/test_lead.rb
@@ -0,0 +1,13 @@
+require_relative '../helper'
+
+class LeadTest < MiniTest::Test
+  include SetupMethods
+
+  def test_post
+    use_vcr do
+      lead = Rentlinx::Lead.new(leadID: 8_552_342, refunded: true, refund_reason: 'I do not like their smell')
+      response = lead.post
+      assert_equal 'premium_refunded_by_user', response['data']['lead_status']
+    end
+  end
+end
