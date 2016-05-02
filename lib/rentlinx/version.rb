db6910357d29257b7bf1db378b7515f1a5c76cb2
Blake Herrington <blake.herrington@appfolio.com>
2016-05-02 11:35:10 -0700

    Bump to 0.10.0

--- lib/rentlinx/version.rb
+++ lib/rentlinx/version.rb
@@ -1,5 +1,5 @@
 # This is the main rentlinx module. All Rentlinx objects
 # and methods are namespaced under this module.
 module Rentlinx
-  VERSION = '0.9.6'.freeze
+  VERSION = '0.10.0'.freeze
 end
