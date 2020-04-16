f4af909e141c415ea1cadb30cf6c5710e65cb2e5
Jiuru Shao <shaojiuru@ucla.edu>
2020-04-16 11:43:11 -0700

    js - update gem version to 0.13.0

--- lib/rentlinx/version.rb
+++ lib/rentlinx/version.rb
@@ -1,5 +1,5 @@
 # This is the main rentlinx module. All Rentlinx objects
 # and methods are namespaced under this module.
 module Rentlinx
-  VERSION = '0.12.0'.freeze
+  VERSION = '0.13.0'.freeze
 end
